a206173a68e8ef4b0c3f9e8a956228628d767956
Leandro Carvalho Batista <leandrocb14@gmail.com>
2019-03-18 22:48:30 -0300

    Criando model.

---
+++ app/Product.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    // Para criar um model via pronpt de comando, basta digitar o comando: php artisan make:model {Nome do Model}
+}
